6c460158fb3e73861ec27c292e07dc5555d18427
armandnjapou <armand2ngassu@gmail.com>
2022-01-22 15:19:33 +0100

    Change DB config

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","");
+                "jdbc:mysql://localhost:3306/prod","parkingsys","6fS@NXUEYDT!e*0x");
     }
 
     public void closeConnection(Connection con){
--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -14,7 +14,7 @@ public class DataBaseTestConfig extends DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test","root","");
+                "jdbc:mysql://localhost:3306/test","parkingsys","6fS@NXUEYDT!e*0x");
     }
 
     public void closeConnection(Connection con){
